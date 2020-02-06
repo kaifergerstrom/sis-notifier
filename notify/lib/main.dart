f60c34347159d3f9818ce03c125bcb9ca715c9de
Kai Fergerstrom <kaifergerstrom@gmail.com>
2020-02-06 09:35:06 -0500

    Simple login and file creation

--- notify/lib/main.dart
+++ notify/lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:convert';
-import 'API.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(new MaterialApp(home: new MyApp()));
+void main() => runApp(new MaterialApp(home: new LoginPage()));
 
 class MyApp extends StatefulWidget {
   @override
@@ -111,5 +112,190 @@ class _MyAppState extends State<MyApp> {
     await flutterLocalNotificationsPlugin.show(
         channel_id, subject, body, platform,
         payload: 'Nitish Kumar Singh is part time Youtuber');
+  }
+
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+// Used for controlling whether the user is loggin or creating an account
+enum FormType {
+  login,
+  register
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final TextEditingController _emailFilter = new TextEditingController();
+  final TextEditingController _passwordFilter = new TextEditingController();
+  String _email = "";
+  String _password = "";
+  FormType _form = FormType.login; // our default setting is to login, and we should switch to creating an account when the user chooses to
+
+  _LoginPageState() {
+    _emailFilter.addListener(_emailListen);
+    _passwordFilter.addListener(_passwordListen);
+  }
+
+  void _emailListen() {
+    if (_emailFilter.text.isEmpty) {
+      _email = "";
+    } else {
+      _email = _emailFilter.text;
+    }
+  }
+
+  void _passwordListen() {
+    if (_passwordFilter.text.isEmpty) {
+      _password = "";
+    } else {
+      _password = _passwordFilter.text;
+    }
+  }
+
+  // Swap in between our two forms, registering and logging in
+  void _formChange () async {
+    setState(() {
+      if (_form == FormType.register) {
+        _form = FormType.login;
+      } else {
+        _form = FormType.register;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: _buildBar(context),
+      body: new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Column(
+          children: <Widget>[
+            _buildTextFields(),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      title: new Text("Simple Login Example"),
+      centerTitle: true,
+    );
+  }
+
+  Widget _buildTextFields() {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Container(
+            child: new TextField(
+              controller: _emailFilter,
+              decoration: new InputDecoration(
+                labelText: 'Email'
+              ),
+            ),
+          ),
+          new Container(
+            child: new TextField(
+              controller: _passwordFilter,
+              decoration: new InputDecoration(
+                labelText: 'Password'
+              ),
+              obscureText: true,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    if (_form == FormType.login) {
+      return new Container(
+        child: new Column(
+          children: <Widget>[
+            new RaisedButton(
+              child: new Text('Login'),
+              onPressed: _loginPressed,
+            ),
+          ],
+        ),
+      );
+    } else {
+      return new Container(
+        child: new Column(
+          children: <Widget>[
+            new RaisedButton(
+              child: new Text('Create an Account'),
+              onPressed: _createAccountPressed,
+            ),
+            new FlatButton(
+              child: new Text('Have an account? Click here to login.'),
+              onPressed: _formChange,
+            )
+          ],
+        ),
+      );
+    }
+  }
+
+  // These functions can self contain any user auth logic required, they all have access to _email and _password
+
+  Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+  // For your reference print the AppDoc directory 
+  print(directory.path);
+  return directory.path;
+}
+
+Future<File> get _localFile async {
+  final path = await _localPath;
+  print(path);
+  return File('$path/user.json');
+}
+
+Future<File> storeUserData(String data) async {
+  final file = await _localFile;
+
+  // Write the file.
+  return file.writeAsString('$data');
+}
+
+Future<String> readCounter() async {
+  try {
+    final file = await _localFile;
+
+    // Read the file.
+    String contents = await file.readAsString();
+
+    return contents;
+  } catch (e) {
+    // If encountering an error, return 0.
+    return "";
+  }
+}
+
+  void _loginPressed () {
+    //print('The user wants to login with $_email and $_password');
+    var jsonData = '{ "username" : "$_email", "password" : "$_password"}';
+    print(jsonData);
+    storeUserData(jsonData);
+    //print("Hi" + readCounter());
+  }
+
+  void _createAccountPressed () {
+    print('The user wants to create an accoutn with $_email and $_password');
+
+  }
+
+  void _passwordReset () {
+    print("The user wants a password reset request sent to $_email");
   }
 }
